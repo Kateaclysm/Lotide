3602ffaa863bbd0f2edc097488a673df62d50998
Katelynn <grayspence2@hotmail.com>
2023-09-20 19:54:22 -0400

    Here's the assertEqual assignment!

---
+++ assertEqual.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    console.log("✅✅✅ Assertion Passed! " + actual + " === " + expected);
+
+  } else {
+
+    console.log("❌❌❌ Assertion FAILED! " + actual + " !== " + expected);
+  }
+
+};
+
+
+
+//TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(256, 908);
+assertEqual("Goofball", "Goofball");
