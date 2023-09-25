ffe347fab852b8a0256ab3de912823b088f92e87
Katelynn <grayspence2@hotmail.com>
2023-09-25 15:27:31 -0400

    adding flatten.js to the repo

---
+++ flatten.js
@@ -0,0 +1,23 @@
+const flatten = function(array1) {
+  let arrayStatus = array1;
+
+  let flattenedArray = undefined;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) === true) {
+    
+      arrayStatus = true;
+
+    }
+   }
+
+  if (arrayStatus === true) {
+
+    flattenedArray = array1.flat();
+  }
+
+  return flattenedArray;
+
+}
+
+console.log(flatten([1, 2, 3, [4, 5, 6], [7, 8, 9]]));
