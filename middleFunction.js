afa295c5e5482978eee829ee139e9a7801f89248
Katelynn <grayspence2@hotmail.com>
2023-09-25 17:37:43 -0400

    Adding the middle function to the repo

---
+++ middleFunction.js
@@ -0,0 +1,22 @@
+const middle = function (inputArray) {
+  let revisedArray = [];
+  let length = inputArray.length;
+  if (length <= 2){
+    return revisedArray;
+  }
+  
+  if (length % 2 === 0) {
+    revisedArray.push(inputArray[length / 2 - 1]);
+    revisedArray.push(inputArray[length / 2]);
+  }
+  if (length % 2 !== 0) {
+    length = Math.round(inputArray.length / 2);
+    revisedArray.push(length);
+  }
+
+  
+  console.log(revisedArray);
+}
+
+
+
