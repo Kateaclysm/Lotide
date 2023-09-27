9806e54ead0b8a52f696b704f84032f289c5f7db
Katelynn <grayspence2@hotmail.com>
2023-09-27 18:35:47 -0400

    adding the 'find key by value' function

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const findKeyByValue = function (object, value) {
+  let keysArray = Object.keys(object)
+  let finalArray = [];
+
+  for (const key of keysArray) {
+    if (object[key] === value) {
+
+      finalArray.push(key);
+
+    }
+  }
+  if (finalArray.length === 0) {
+    return undefined;
+  }
+
+  return finalArray;
+
+};
+
+
+ 
+// TEST OBJECT
+
+const exampleObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: "string",
+  e: false
+}
+// TEST CASES 
+
+console.log(findKeyByValue(exampleObject, "Kate"));
+console.log(findKeyByValue(exampleObject, 1));
+console.log(findKeyByValue(exampleObject, 2));
+console.log(findKeyByValue(exampleObject, 3));
+console.log(findKeyByValue(exampleObject, false));
+console.log(findKeyByValue(exampleObject, "Goop"));
