287c6455bab8304c1a30109970ef6a05585ea240
Katelynn <grayspence2@hotmail.com>
2023-09-26 20:44:49 -0400

    adding the letterPositions. function

---
+++ letterPositions.js
@@ -0,0 +1,24 @@
+const letterPositions = function (sentence) {
+  
+  const result = {};
+  //code to update results goes here
+  const newSentence = Array.from(sentence);
+  
+  for (let i = 0; i < newSentence.length; i++) {
+    
+    if (newSentence[i] !== " ") {
+
+      if(result[newSentence[i]]) {
+
+      result[newSentence[i]].push(i);
+
+    } else {
+        result[newSentence[i]] = [i]
+      }
+    }
+  }
+return result;
+};
+console.log(letterPositions("My Name Is Kate"))
+
+console.log(letterPositions("aabbcc"));
