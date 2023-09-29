dc32219b5c6ff6fd83e349a670c40334ce109d5c
Katelynn <grayspence2@hotmail.com>
2023-09-28 21:00:11 -0400

    adding the takeUntil function!

---
+++ takeUntil.js
@@ -0,0 +1,17 @@
+const testArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+const testArray2 = ["Having", "a", "very", ",", "very", "nice", "day"];
+
+
+const takeUntil = function(array, callback) {
+  returnedArray = [];
+  for (item of array) {
+    if (callback(item) === false) {
+      returnedArray.push(item)
+    }else break;
+  }
+  console.log(returnedArray);
+};
+
+
+takeUntil(testArray1, (item) => item >= 6 );
+takeUntil(testArray2, (item) => item === ",")
