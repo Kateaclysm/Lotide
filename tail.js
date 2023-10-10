e9c91963117ab4679e7c11917b56d0cd6e993f06
Katelynn <grayspence2@hotmail.com>
2023-10-10 18:04:16 -0400

    Refactored Tail function and added independent testing file for tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-
-    console.log(`✅✅✅ Assertion Passed! ${actual}  ===  ${expected}`);
-
-  } else {
-
-    console.log(`❌❌❌ Assertion FAILED! ${actual} !== ${expected}!`);
-  }
-
-};
-
 //The Tail function takes the given array and uses the slice method to remove the first item, creating a shallow copy array that only contains the tail of the given array.
 const tail = function(array) {
 
@@ -18,13 +5,4 @@ const tail = function(array) {
   return arrayTail;
 };
 
-//Testing the assertEqual function with the tail function here:
-// -------------------------------------------------------------------
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-//The line above is creating a variable that stores the tail function's result.
-
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
