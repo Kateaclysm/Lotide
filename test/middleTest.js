41b6944cad5d0c64f95f2a9dab48948008f383f0
Katelynn <grayspence2@hotmail.com>
2023-10-10 18:52:52 -0400

    added chai/mocha testing to both the middle function and tail function test files.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,12 @@
 const middle = require(`../middleFunction`);
 const assertArraysEqual = require(`../assertArraysEqual`);
+const assert = require('chai').assert;
 
-
-console.log(middle([1,2,3,4,5,6]));
-console.log(middle([1,2,3,4,5]));
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
+describe("#middle", () => {
+    it("returns [2,3] for [1, 2, 3, 4]", () => {
+      assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+    });
+    it("returns [3] for [1,2,3,4,5]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+      });
+  });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,9 @@
 const assertEqual = require('../assertEqual');
 const tail = require(`../tail`);
+const assert = require('chai').assert;
 
-console.log(tail(["Hello", "Lighthouse", "Labs"]));
+describe("#tail", () => {
+    it("returns [2,3] for [1, 2, 3]", () => {
+      assert.deepEqual(tail([1, 2, 3]), [2,3]);
+    });
+  });
