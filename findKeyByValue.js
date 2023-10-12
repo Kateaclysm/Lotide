65336f0f99e9d015f2caff06608b195cde63f64f
Katelynn <grayspence2@hotmail.com>
2023-10-12 17:33:15 -0400

    removed commented code of findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -18,21 +18,3 @@ const findKeyByValue = function (object, value) {
 };
 
 module.exports = findKeyByValue;
- 
-// TEST OBJECT
-
-/*const exampleObject = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: "string",
-  e: false
-}*/
-// TEST CASES 
-/*
-console.log(findKeyByValue(exampleObject, "Kate"));
-console.log(findKeyByValue(exampleObject, 1));
-console.log(findKeyByValue(exampleObject, 2));
-console.log(findKeyByValue(exampleObject, 3));
-console.log(findKeyByValue(exampleObject, false));
-console.log(findKeyByValue(exampleObject, "Goop"));*/
